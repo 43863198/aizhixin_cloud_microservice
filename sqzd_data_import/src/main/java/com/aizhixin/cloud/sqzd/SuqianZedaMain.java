ac60685a6fb71c7b87163fd37fdff645b06765fb
潘震 <panzhen@aizhixin.com>
2018-11-22 15:15:24 +0800

    宿迁泽达数据导入项目

---
+++ sqzd_data_import/src/main/java/com/aizhixin/cloud/sqzd/SuqianZedaMain.java
@@ -0,0 +1,13 @@
+package com.aizhixin.cloud.sqzd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableScheduling
+//@EnableAsync
+public class SuqianZedaMain {
+	public static void main(String[] args) {
+		SpringApplication.run(SuqianZedaMain.class, args);
+	}
+}
